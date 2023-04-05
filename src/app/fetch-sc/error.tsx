91f253b432ff75d18b0eea8574fc4e2770b3d763
fo-ji <fumiya1753@gmail.com>
2023-04-05 12:23:18 +0900

    fix

--- src/app/fetch-sc/error.tsx
+++ src/app/fetch-sc/error.tsx
@@ -1,7 +1,6 @@
 'use client';
-export default function Error({ error }: { error: Error }) {
-  console.log({ error });
 
+export default function Error({ error }: { error: Error }) {
   return (
     <div>
       <p className="mt-6 text-center text-red-500">
