2c5f4cd3453d07c161df5475b3883f899ed6a445
fo-ji <fumiya1753@gmail.com>
2023-04-05 12:50:46 +0900

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'yarn start',
     url: baseURL,
-    timeout: 120 * 1000,
+    // timeout: 120 * 1000,
     reuseExistingServer: true,
   },
   globalSetup: './e2e/config/globalSetup.ts',
