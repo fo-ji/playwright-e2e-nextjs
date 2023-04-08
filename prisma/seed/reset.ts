eeb9b134e7fe82e0ed6259ccd7f1014db4b6e8e3
fo-ji <fumiya1753@gmail.com>
2023-04-08 15:13:05 +0900

    [WIP]add crud test

--- prisma/seed/reset.ts
+++ prisma/seed/reset.ts
@@ -3,5 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const reset = async () => {
+  await prisma.task.deleteMany();
   await prisma.note.deleteMany();
+  await prisma.user.deleteMany();
 };
